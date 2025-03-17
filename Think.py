0ecb9dd68ab3392a71789bac72939d6d65aa0088
Nicholas Tate <98060238+Kinny-B@users.noreply.github.com>
2025-03-17 00:21:39 -0500

    Update Think.py

--- Think.py
+++ Think.py
@@ -1,9 +1,7 @@
 from ollama import generate
-# Large-Language-Model
-direction, response = ""
-
+# Large-Language-Model Ollama Distilled 'DeepSeek-R1:1b'
 # 'Think' of a response and direction from prompt and objects using DeepSeek LLM
 def think(dpsk, prompt, objects):
     print("\nthinking...\n")
     prompt += ",detected objects: {objects}"
-    response = generate(dpsk, prompt)
+    return generate(dpsk, prompt)
