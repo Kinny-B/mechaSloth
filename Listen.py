877f744519b7732e89fc82334d0281266e99e619
Nicholas Tate <98060238+Kinny-B@users.noreply.github.com>
2025-03-21 23:45:56 -0500

    added keyboard interupt. maybe fixed vosk
    vosk bug with scope of prompt

--- Listen.py
+++ Listen.py
@@ -8,49 +8,21 @@ CHANNELS = 1
 FORMAT = pyaudio.paInt16
 SILENCE_THRESHOLD = 300
 LEN = 5
-global prompt = 'hello mechasloth, '
-
-# capture audio
-def audio():
-	listening = 1
+def listen(vosk_m):
+	# capture audio
 	speech = ' '
 	mic = pyaudio.PyAudio()
 	stream = mic.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-#	frames = []
-#	speech_buffer = []
-#	speech_start = 0
 	print("\nlistening...\n")
 	for i in range(int(LEN*RATE/CHUNK)): #go for a LEN seconds
-			speech = stream.read(CHUNK)
-#	while(listening):
-#		rms = numpy.sqrt(numpy.mean(audio_frame**2))
-#		# voice activity check
-#		if(rms > SILENCE_THRESHOLD):
-#			if not(listening):
-#				listening = True
-#				speech_start = len(frames)
-#				speech_buffer.append(speech)
-#		elif(listening):
-#			if(( (len(frames) - speech_start) / RATE > MIN_SPEECH)):
-#				frames.extend(speech_buffer)
-#				break
-#			speech_buffer = []
-#			listening = False
-#		frames.append(speech)
-#		audio_data = b''.join(frames)
+		speech = stream.read(CHUNK)
+	print("\t...speech recognized\n")
 	stream.stop_stream()
 	stream.close()
 	mic.terminate()
-	return speech
-# use captured audio to generate prompt
-def speech2text(audio_data, vosk_m):
-	if (vosk_m.AcceptWaveform(audio_data)):
+	# use captured audio to generate prompt
+	prompt = ' '
+	if (vosk_m.AcceptWaveform(speech)):
 		prompt += vosk_m.Result()
-		print("\t...speech recognized\n")
-	return prompt
-# get audio then prompt
-def listen(vosk_m):
-	audio_data = audio()
-	prompt = speech2text(audio_data, vosk_m)
-	print("\t...prompt generated\n")
+		print("\t...prompt generated\n")
 	return prompt
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import Load, Listen, Look, Move, Speak, Think
 # main
 if __name__ == "__main__":
-    running = True
-    prompt = ""
+    running = 1
     # Load AI Models
     dpsk = Load.getDeepseek()
     vosk = Load.getVosk()
@@ -10,15 +9,19 @@ if __name__ == "__main__":
     yolo = Load.getYolo()
     # Load Camera
     picam2 = Load.setup_camera()
-    # run main loop
-    while(running):
-        # 'Listen' for speech and generate prompt string
-        prompt = Listen.listen(vosk)
-        # 'Look' for and identify objects list
-        objects = Look.look(yolo, picam2)
-        # 'Think' and generate a response string
-        response = Think.think(dpsk, prompt, objects)
-        # 'Speak' the generated respone
-        Speak.speak(pip, response)
-        # 'Move' a direction by parsing the response for commands
-        #Move.move(response)
+    # run main loop with catch keyboard interupt
+    try:
+        while(running):
+            # 'Listen' for speech and generate prompt string
+            prompt = Listen.listen(vosk)
+            # 'Look' for and identify objects list
+            objects = Look.look(yolo, picam2)
+            # 'Think' and generate a response string
+            response = Think.think(dpsk, prompt, objects)
+            # 'Speak' the generated response
+            Speak.speak(pip, response)
+            # 'Move' a direction by parsing the response for commands
+            #Move.move(response)
+    except KeyboardInterrupt:
+        print("Interupted! exiting")
+        running = 0
