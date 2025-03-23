c3d0b0f3e247f1869d9bec17ddb244defecd5505
Nicholas Tate <98060238+Kinny-B@users.noreply.github.com>
2025-03-23 03:54:00 -0500

    Fixed Listen, now works pretty good

--- Listen.py
+++ Listen.py
@@ -1,34 +1,40 @@
 import pyaudio
-import numpy
-import vosk
+import json
+from vosk import KaldiRecognizer
 # speech to text, using vosk and kaldi
 RATE = 16000
 CHUNK = 4096
 CHANNELS = 1
 FORMAT = pyaudio.paInt16
-SILENCE_THRESHOLD = 300
-LEN = 5
+LEN = 10
+# get audio then prompt
 def listen(voskRec):
-    # capture audio
+	# capture audio
+	data = bytearray()
+	fragments = []
 	mic = pyaudio.PyAudio()
 	stream = mic.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 	print("\nlistening...\n")
-	speech = bytearray()
-	# try to listen
 	try:
-		for _ in range(int(LEN*RATE/CHUNK)):  # 5 seconds
-			data = stream.read(CHUNK, exception_on_overflow=False)
-			speech.extend(data)
-            # Process incrementally
-			if voskRec.AcceptWaveform(data):
-				result = voskRec.Result()
-				print("Partial result:", result)
-                
+		for _ in range(int(LEN*RATE/CHUNK)): #go for a LEN seconds
+			speech = stream.read(CHUNK, exception_on_overflow=False)
+			data.extend(speech)
+			# process incrementally
+			if (voskRec.AcceptWaveform(speech)):
+				result = json.loads(voskRec.Result())
+				if 'text' in result:
+					fragments.append(result['text'].strip())
+		# get final result
+		fullResult = json.loads(voskRec.FinalResult())
+		if 'text' in fullResult:
+			fragments.append(fullResult['text'].strip())
+		# assemble prompt
+		prompt = " ".join(fragments)
+		prompt = f"{prompt.strip()}"
 	finally:
 		stream.stop_stream()
 		stream.close()
 		mic.terminate()
-    # Get final result
-	result = voskRec.FinalResult()
-	print("Final result:", result)
-	return result
+	# test print prompt then return
+	print("\t...speech recognized:\n", prompt)
+	return prompt
