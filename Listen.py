3a54b347895e80ab3b949c3faabf4546689573e0
Nicholas Tate <98060238+Kinny-B@users.noreply.github.com>
2025-03-22 23:10:44 -0500

    changed Listen
    tryin it different way

--- Listen.py
+++ Listen.py
@@ -3,26 +3,32 @@ import numpy
 import vosk
 # speech to text, using vosk and kaldi
 RATE = 16000
-CHUNK = 2**5
+CHUNK = 4096
 CHANNELS = 1
 FORMAT = pyaudio.paInt16
 SILENCE_THRESHOLD = 300
 LEN = 5
-def listen(vosk_m):
-	# capture audio
-	speech = ' '
+def listen(voskRec):
+    # capture audio
 	mic = pyaudio.PyAudio()
 	stream = mic.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 	print("\nlistening...\n")
-	for i in range(int(LEN*RATE/CHUNK)): #go for a LEN seconds
-		speech = stream.read(CHUNK)
-	print("\t...speech recognized\n")
-	stream.stop_stream()
-	stream.close()
-	mic.terminate()
-	# use captured audio to generate prompt
-	prompt = ' '
-	if (vosk_m.AcceptWaveform(speech)):
-		prompt += vosk_m.Result()
-		print("\t...prompt generated\n")
-	return prompt
+	speech = bytearray()
+	# try to listen
+	try:
+		for _ in range(int(LEN*RATE/CHUNK)):  # 5 seconds
+			data = stream.read(CHUNK, exception_on_overflow=False)
+			speech.extend(data)
+            # Process incrementally
+			if voskRec.AcceptWaveform(data):
+				result = voskRec.Result()
+				print("Partial result:", result)
+                
+	finally:
+		stream.stop_stream()
+		stream.close()
+		mic.terminate()
+    # Get final result
+	result = voskRec.FinalResult()
+	print("Final result:", result)
+	return result
