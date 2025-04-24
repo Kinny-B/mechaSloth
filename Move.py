e7f1108693eb73af6e0e742d61019052334114b9
Nicholas Tate <98060238+Kinny-B@users.noreply.github.com>
2025-04-23 19:29:04 -0500

    altered move

--- Move.py
+++ Move.py
@@ -10,7 +10,7 @@ MOVE_DURATION = 5
 # parse response for movement
 def move(response):
     print("\nseeking path to move...\n")
-    if any(cmd in response for cmd in ["forward", "ahead", "move", "drive", "north"]):
+    if any(cmd in response for cmd in ["forward", "ahead", "north"]):
         move_forward()
     elif any(cmd in response for cmd in ["backward", "behind", "reverse", "south"]):
         move_backward()
@@ -27,30 +27,30 @@ def stop_motors():
     kit.motor3.throttle = 0
     kit.motor4.throttle = 0
 def move_forward():
-    kit.motor1.throttle = 1
-    kit.motor2.throttle = 1
-    kit.motor3.throttle = 1
-    kit.motor4.throttle = 1
+    kit.motor1.throttle = 0.8
+    kit.motor2.throttle = 0.8
+    kit.motor3.throttle = 0.8
+    kit.motor4.throttle = 0.8
     time.sleep(MOVE_DURATION)
     stop_motors()
 def move_backward():
-    kit.motor1.throttle = -1
-    kit.motor2.throttle = -1
-    kit.motor3.throttle = -1
-    kit.motor4.throttle = -1
+    kit.motor1.throttle = -0.8
+    kit.motor2.throttle = -0.8
+    kit.motor3.throttle = -0.8
+    kit.motor4.throttle = -0.8
     time.sleep(MOVE_DURATION)
     stop_motors()
 def turn_left():
-    kit.motor1.throttle = 0.2
-    kit.motor2.throttle = 0.2
-    kit.motor3.throttle = 1
-    kit.motor4.throttle = 1
+    kit.motor1.throttle = -0.3
+    kit.motor2.throttle = -0.3
+    kit.motor3.throttle = 0.3
+    kit.motor4.throttle = 0.3
     time.sleep(MOVE_DURATION)
     stop_motors()
 def turn_right():
-    kit.motor1.throttle = 1
-    kit.motor2.throttle = 1
-    kit.motor3.throttle = 0.2
-    kit.motor4.throttle = 0.2
+    kit.motor1.throttle = 0.3
+    kit.motor2.throttle = 0.3
+    kit.motor3.throttle = -0.3
+    kit.motor4.throttle = -0.3
     time.sleep(MOVE_DURATION)
     stop_motors()
