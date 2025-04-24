038389491d964ac223cbd9ae9e75a8b2a1afb5f5
Nicholas Tate <98060238+Kinny-B@users.noreply.github.com>
2025-04-24 01:31:58 -0500

    seems to work now
    Need to adjust the camera a little more its still blurry

--- Look.py
+++ Look.py
@@ -1,31 +1,10 @@
 #Look Function
-from ultralytics import YOLO
 import cv2
-
 def look(yolo, picam2):
-    """
-    Detects objects in the given image using a YOLO model.
-    
-    Args:
-        image_path (str): Path to the image file.
-    
-    Returns:
-        list of dict: A list containing detected objects with their class names and confidence scores.
-    """
+    # take picture
     picam2.capture_file("image.jpg")
-    results = yolo("/home/kinbok/mechasloth/src/image.jpg")
-    
-    # Get the first result (assumes batch size = 1)
-    detections = results[0]
-
-    objects = []
-    for box in detections.boxes:
-        class_id = int(box.cls)
-        class_name = model.names[class_id]
-        confidence = float(box.conf)
-        objects.append({
-            "name": class_name,
-            "confidence": confidence
-        })
-    
-    return objects
+    # Load image with OpenCV
+    image = cv2.imread("/home/kinbok/mechasloth/src/image.jpg")
+    image = cv2.rotate(image, cv2.ROTATE_180)
+    # return Inference
+    return yolo(image)
--- testLook.py
+++ testLook.py
@@ -3,6 +3,7 @@ import Load, Look
 if __name__ == "__main__":
     yolo = Load.getYolo()
     picam2 = Load.setup_camera()
-    # 'Look' for and identify objects list
-    objects = Look.look(yolo, picam2)
-    print(objects)
+    # 'Look' for and identify objects, print results
+    results = Look.look(yolo, picam2)
+    print(results.xyxy[0],"\n\n")
+    results.print()
